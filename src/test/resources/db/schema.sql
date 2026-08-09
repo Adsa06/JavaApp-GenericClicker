73b4f95c30b5911e8afe67bcd8af9fe0161124b8
Aitor de Santos Amoros <soyadsa06@gmail.com>
2026-08-09 23:25:52 +0200

    Add JSON data files for achievements, buildings, and upgrades; create SQL schema and internationalization properties

---
+++ src/test/resources/db/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS achievements (
+    id TEXT PRIMARY KEY,
+    creation_date TEXT DEFAULT (datetime('now', 'localtime'))
+);
+
+-- Acceder solo a la fila 1
+CREATE TABLE IF NOT EXISTS stats (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    actualCounter INTEGER NOT NULL DEFAULT 0,
+    totalCounter INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT OR IGNORE INTO stats (id, actualCounter, totalCounter)
+VALUES (1, 0, 0);
+
+CREATE TABLE IF NOT EXISTS buildings (
+    id TEXT PRIMARY KEY,
+    level INTEGER NOT NULL DEFAULT 0,
+    cost INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS upgrades (
+    id TEXT PRIMARY KEY
+);
+
+-- Acceder solo a la fila 1
+CREATE TABLE IF NOT EXISTS settings (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    selectedLanguage TEXT NOT NULL DEFAULT 'en'
+);
+
+INSERT OR IGNORE INTO settings (id, selectedLanguage)
+VALUES (1, 'en');
